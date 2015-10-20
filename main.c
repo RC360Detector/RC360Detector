0640a0bbf3a72fe0ebfacedfd84083964f6655cf
Matt Whiteside <matt@wemash.com>
2015-10-20 10:39:32 -0700

    Adding empty version of main.c for the atmega 328p.

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * RC360Detector.c
+ *
+ * Created: 10/20/2015 9:43:03 AM
+ * Author : Matt
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
