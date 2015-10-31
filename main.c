4021dc58ade00bfe35d658d287e5a34a3db9757c
Matt Whiteside <matt@wemash.com>
2015-10-31 12:52:40 -0700

    Code works with proximity sensor.

--- main.c
+++ main.c
@@ -18,6 +18,21 @@
 */
 
 #include <avr/io.h>
+#include <stdint.h>
+
+/* Which analog pin we want to read from.  The pins are labeled "ADC0"
+ * "ADC1" etc on the pinout in the data sheet.  In this case ADC_PIN
+ * being 0 means we want to use ADC0.  On the ATmega328P this is also
+ * the same as pin PC0 */
+#define ADC_PIN			0
+#define ADC_THRESHOLD	110
+#define	LED_PIN			PB0
+/* This function just keeps the reading code out of the loop itself.
+ * It takes the analog pin number as a parameter and returns the
+ * analog reading on that pin as a result.
+ *
+ * Look for its definition below main. */
+uint16_t adc_read(uint8_t adcx);
 
 //Define functions
 //======================
@@ -28,7 +43,7 @@ void delay_ms(uint16_t x); //General purpose delay
 int main (void)
 {
     ioinit(); //Setup IO pins and defaults
-
+/*
     while(1)
     {
 		PORTC = 0xFF;
@@ -41,16 +56,29 @@ int main (void)
 		PORTD = 0x00;
 		delay_ms(500);
     }
-   
+  */
+	for (;;) {
+
+		if (adc_read(ADC_PIN) > ADC_THRESHOLD){
+			PORTB |= _BV(LED_PIN);
+		} else {
+			PORTB &= ~_BV(LED_PIN);
+		}
+	} 
     return(0);
 }
 
 void ioinit (void)
 {
     //1 = output, 0 = input
-    DDRB = 0b11111111; //All outputs
-    DDRC = 0b11111111; //All outputs
-    DDRD = 0b11111110; //PORTD (RX on PD0)
+    //DDRB = 0b11111111; //All outputs
+    //DDRC = 0b11111111; //All outputs
+    //DDRD = 0b11111110; //PORTD (RX on PD0)
+	DDRB  |= _BV(LED_PIN);
+	//ADCSRA |= _BV(ADEN);
+	ADMUX |= (1<<REFS0);
+	//set prescaller to 128 and enable ADC
+	ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);
 }
 
 //General short delays
@@ -66,3 +94,26 @@ void delay_ms(uint16_t x)
   }
 }
 
+
+uint16_t adc_read(uint8_t adcx) {
+	/* adcx is the analog pin we want to use.  ADMUX's first few bits are
+	 * the binary representations of the numbers of the pins so we can
+	 * just 'OR' the pin's number with ADMUX to select that pin.
+	 * We first zero the four bits by setting ADMUX equal to its higher
+	 * four bits. */
+	ADMUX	&=	0xf0;
+	ADMUX	|=	adcx;
+
+	/* This starts the conversion. */
+	ADCSRA |= _BV(ADSC);
+
+	/* This is an idle loop that just wait around until the conversion
+	 * is finished.  It constantly checks ADCSRA's ADSC bit, which we just
+	 * set above, to see if it is still set.  This bit is automatically
+	 * reset (zeroed) when the conversion is ready so if we do this in
+	 * a loop the loop will just go until the conversion is ready. */
+	while ( (ADCSRA & _BV(ADSC)) );
+
+	/* Finally, we return the converted value to the calling function. */
+	return ADC;
+}
