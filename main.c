39511ce819dd911f00d94405863f2b9b04fd2d3b
Matt Whiteside <matt.whiteside@gmail.com>
2015-11-07 12:35:03 -0800

    Added interrupt code.

--- main.c
+++ main.c
@@ -2,26 +2,17 @@
  * RC360Detector.c
  *
  * Created: 10/20/2015 9:43:03 AM
- * Author : Matt
+ * 
  */ 
 
-/*
-    5-10-07
-    Copyright Spark Fun Electronics© 2007
-    Nathan Seidle
-    nathan at sparkfun.com
-    
-    ATmega168
-	
-	Example Blink
-	Toggles all IO pins at 1Hz
-*/
+
 
 #define F_CPU 8000000
 
 #include <avr/io.h>
 #include <stdint.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
 /* Which analog pin we want to read from.  The pins are labeled "ADC0"
  * "ADC1" etc on the pinout in the data sheet.  In this case ADC_PIN
@@ -30,6 +21,10 @@
 #define ADC_PIN			0
 #define ADC_THRESHOLD	110
 #define	LED_PIN			PB0
+#define	INT_LED			PB1
+
+int x = 0;
+
 /* This function just keeps the reading code out of the loop itself.
  * It takes the analog pin number as a parameter and returns the
  * analog reading on that pin as a result.
@@ -52,45 +47,25 @@ void Wait(){
 		_delay_loop_2(0);
 	}
 }
-/*
-int main (void){
-    ioinit(); //Setup IO pins and defaults
-	
 
-	// set prescaler to 8 and starts PWM
-
-//	for (;;) {
-
-//		if (adc_read(ADC_PIN) > ADC_THRESHOLD){
-//			PORTB |= _BV(LED_PIN);
-//		} else {
-//			PORTB &= ~_BV(LED_PIN);
-//		}
-//	} 
-
-	init_pwm();
-	while(1){
-//		OCR1A=316;  //90 degree
-//		Wait();
-//		OCR1A=97;   //0 degree
-//		Wait();
-//		OCR1A=535;  //180 degree
-//		Wait();
-	}
-    return(0);
-}
-*/
 
 
 
 int main(void){
 	
+	DDRD &= ~(1 << DDD2);     // Clear the PD2 pin
+	// PD2 (PCINT0 pin) is now an input
+
+	
+	EICRA |= (1 << ISC00);    // set INT0 to trigger on ANY logic change
+	EICRA &= ~(1 << ISC01);
+	EIMSK |= (1 << INT0);     // Turns on INT0
+
 	DDRD |= (1 << DDD6);
 	// PD6 is now an output
 
 	OCR0A = 128;
 	// set PWM for 50% duty cycle
-
 
 	TCCR0A |= (1 << COM0A1);
 	// set none-inverting mode
@@ -102,24 +77,17 @@ int main(void){
 	// set prescaler to 8 and starts PWM
 
 	ioinit();
+	sei();                    // turn on interrupts
+
 	for (;;){
 		if (adc_read(ADC_PIN) > ADC_THRESHOLD){
 			PORTB |= _BV(LED_PIN);
 		} else {
 			PORTB &= ~_BV(LED_PIN);
 		}
-		// we have a working Fast PWM
 	}
 }
 
-void init_pwm(void){
-	TCCR1A|=(1<<COM1A1)|(1<<COM1B1)|(1<<WGM11);        //NON Inverted PWM
-	TCCR1B|=(1<<WGM13)|(1<<WGM12)|(1<<CS11)|(1<<CS10); //PRESCALER=64 MODE 14(FAST PWM)
-	ICR1=4999;  //fPWM=50Hz
-	DDRD |= (1 << DDD6);
-	
-	//DDRD|=(1<<PD4)|(1<<PD5);
-}
 
 void ioinit (void)
 {
@@ -130,18 +98,7 @@ void ioinit (void)
 	ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);
 }
 
-//General short delays
-void delay_ms(uint16_t x)
-{
-  uint8_t y, z;
-  for ( ; x > 0 ; x--){
-    for ( y = 0 ; y < 90 ; y++){
-      for ( z = 0 ; z < 6 ; z++){
-        asm volatile ("nop");
-      }
-    }
-  }
-}
+
 
 
 uint16_t adc_read(uint8_t adcx) {
@@ -165,4 +122,11 @@ uint16_t adc_read(uint8_t adcx) {
 
 	/* Finally, we return the converted value to the calling function. */
 	return ADC;
+}
+
+
+ISR (INT0_vect)
+{
+	PORTB ^= _BV(INT_LED);
+	_delay_ms(500);
 }
