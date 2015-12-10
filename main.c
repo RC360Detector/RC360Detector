e21c4503b64a13e8179ef99db148ed45dac27c1f
Matt Whiteside <matt.whiteside@gmail.com>
2015-12-09 19:10:40 -0800

    Adding licence and final version of code.

--- main.c
+++ main.c
@@ -1,9 +1,26 @@
 /*
  * RC360Detector.c
  *
- * Created: 10/20/2015 9:43:03 AM
- * 
- */ 
+ * Software to control an object proximity detection system 
+ * consisting of an IR proximity sensor, LEDs, a buzzer, and a servo.
+ *
+ *
+ *
+ * GNU License:
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 
 
@@ -15,14 +32,15 @@
 #include <avr/interrupt.h>
 
 /* Which analog pin we want to read from.  The pins are labeled "ADC0"
- * "ADC1" etc on the pinout in the data sheet.  In this case ADC_PIN
+ * "ADC1" etc on the pin out in the data sheet.  In this case ADC_PIN
  * being 0 means we want to use ADC0.  On the ATmega328P this is also
  * the same as pin PC0 */
 #define ADC_PIN			0
 #define ADC_THRESHOLD	110
-#define ADC_NEAR		275
+#define ADC_NEAR		125
 #define ADC_FAR			55
 #define	LED_PIN			PB0
+#define	BUZZ			PB2
 #define LED0			PD5			//blue
 #define LED1			PD4
 #define LED2			PD7
@@ -61,31 +79,36 @@ int main(void){
 	// PD2 (PCINT0 pin) is now an input
 	
 	
-	EICRA &= ~(1 << ISC00);    // set INT0 to trigger on falling edge of PD2
-	EICRA |= (1 << ISC01);
+	EICRA &= ~(1 << ISC01);    // set INT0 to trigger on any state change
+	EICRA |= (1 << ISC00);
 	
 	EIMSK |= (1 << INT0);     // Turns on INT0
-
-
+	
+	
 	DDRD |= (1 << DDD6) | //servo
-			(1 << DDD7) | //red LED			
-			(1 << DDD5) | //blue LED
-			(1 << DDD4);  //green LED
-
+	(1 << DDD7) | //red LED
+	(1 << DDD5) | //blue LED
+	(1 << DDD4);  //green LED
+	
+	DDRB |= (1 << DDB2) |
+	(1 << DDB1);
+	
+	
 	OCR0A = 128;
 	// set PWM for 50% duty cycle
-
-
+	
+	
 	TCCR0A |= (1 << WGM01) | (1 << WGM00);
 	// set fast PWM Mode
-
 	
-
+	
+	
 	ioinit();
 	sei();                    // turn on interrupts
 	PORTB |= _BV(LED2);
 	PORTB |= _BV(LED1);
 	PORTB |= _BV(LED0);
+	
 	
 	//intialize pwm to off
 	TCCR0A &= ~(1 << COM0A1);// set none-inverting mode
@@ -96,29 +119,33 @@ int main(void){
 			PORTD |= _BV(LED0);			//blue
 			PORTD &= ~_BV(LED1);
 			PORTD &= ~_BV(LED2);
+			PORTB |= _BV(BUZZ);
 		}
 		else if ((adc_read(ADC_PIN) < ADC_NEAR) && (adc_read(ADC_PIN) > ADC_FAR))
 		{
 			PORTD &= ~_BV(LED0);
 			PORTD |= _BV(LED1);
 			PORTD &= ~_BV(LED2);
+			PORTB &= ~_BV(BUZZ);
 		}
 		else if ((adc_read(ADC_PIN) < ADC_FAR))
 		{
 			PORTD &= ~_BV(LED0);
 			PORTD &= ~_BV(LED1);
 			PORTD |= _BV(LED2);
+			PORTB &= ~_BV(BUZZ);
 		}
-
+		
 	}
-
+	
 }
 
 
 void ioinit (void)
 {
-
+	
 	DDRB  |= _BV(LED_PIN);
+	
 	ADMUX |= (1<<REFS0);
 	//set prescaller to 128 and enable ADC
 	ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);
@@ -135,17 +162,17 @@ uint16_t adc_read(uint8_t adcx) {
 	 * four bits. */
 	ADMUX	&=	0xf0;
 	ADMUX	|=	adcx;
-
+	
 	/* This starts the conversion. */
 	ADCSRA |= _BV(ADSC);
-
+	
 	/* This is an idle loop that just wait around until the conversion
 	 * is finished.  It constantly checks ADCSRA's ADSC bit, which we just
 	 * set above, to see if it is still set.  This bit is automatically
 	 * reset (zeroed) when the conversion is ready so if we do this in
 	 * a loop the loop will just go until the conversion is ready. */
 	while ( (ADCSRA & _BV(ADSC)) );
-
+	
 	/* Finally, we return the converted value to the calling function. */
 	return ADC;
 }
@@ -153,14 +180,17 @@ uint16_t adc_read(uint8_t adcx) {
 
 ISR (INT0_vect)
 {
-	//PORTB ^= _BV(INT_LED);
-	if (PIND & (1<<PD2)){		
+	//PORTB ^= _BV(LED);
+	if (PIND & (1<<PD2)){
 		TCCR0A |= (1 << COM0A1);// set none-inverting mode
-		TCCR0B |= (1 << CS02);			
+		TCCR0B |= (1 << CS01);
+		PORTB |= _BV(INT_LED);
+		
 	} else {
 		// set none-inverting mode
 		TCCR0A &= ~(1 << COM0A1);// set none-inverting mode
-		TCCR0B &= ~(1 << CS02);
+		TCCR0B &= ~(1 << CS01);
+		PORTB &= ~_BV(INT_LED);
 		
 	}
 	// set prescaler to 8 and starts PWM
